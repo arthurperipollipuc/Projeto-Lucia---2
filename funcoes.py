eb578fbfeda341fb03d3f4843dc32e59d3ea1eca
arthurperipollipuc <arthur.ps3@puccampinas.edu.br>
2026-05-25 09:22:25 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -46,11 +46,75 @@ def cadastro_pedidos():
         id_pedido = input("Digite o Id do pedido: ")
 
     
-    id_entregador= int(input('Digite o id do entregador: '))
+    id_entregador = input("Digite o Id do entregador: ")
+    while len(id_entregador) != 4 or not id_entregador[:].isdigit():
+        print("Id do pedido deve conter 4 dígitos")
+        id_entregador = input("Digite o Id do pedido: ")
+
 
     lista = [nome_cliente, endereco, prioridade, descricao, status, id_pedido, id_entregador]
     return lista
+
+def cadastro_entregador():
+
+    nome_entregador = input("Digite o Nome do Entregador: ")
+
+    veiculo=None
+    while veiculo == None:
+        print(f"\n====VEÍCULO====")
+        print("1- Carro")
+        print("2- Van")
+        print("3- Moto")
+        v = int(input("Selecione o veículo: "))
+        if(v==1):
+            veiculo = 'Carro'
+        elif(v==2):
+            veiculo = 'Van'
+        elif(v==3):
+            veiculo = 'Moto'
+        else:
+            print("Selecione uma das opções: ")
+            veiculo=None
     
+    id_entregador = input("Digite o Id do entregador: ")
+    while len(id_entregador) != 4 or not id_entregador[:].isdigit():
+        print("Id do pedido deve conter 4 dígitos")
+        id_entregador = input("Digite o Id do pedido: ")
+
+    id_pedido = input("Digite o Id do pedido: ")
+    while len(id_pedido) != 5 or not id_pedido[0].isalpha() or not id_pedido[1:].isdigit():
+        print("Id do pedido deve conter 5 caracteres, sendo a primeira letra e as demais números.")
+        id_pedido = input("Digite o Id do pedido: ")
+
+    disponibilidade=None
+    while disponibilidade == None:
+        print(f"\n====DISPONIBILIDADE====")
+        print("1- Manhã")
+        print("2- Tarde")
+        print("3- Noite")
+        d = int(input("Selecione a Disponibilidade: "))
+        if(d==1):
+            disponibilidade = 'Manhã'
+        elif(d==2):
+            disponibilidade = 'Tarde'
+        elif (d==3):
+            disponibilidade= 'Noite'
+        else:
+            print("Selecione uma das opções")
+            disponibilidade== None
+    
+
+    lista = [nome_entregador, veiculo, id_entregador, id_pedido, disponibilidade]
+    return lista
+
+def listar_entregadores(lista_ent):
+    for i in range(len(lista_ent)):
+        print(lista_ent[i])
+
+def listar_pedidos(lista_pedidos):
+    for i in range(len(lista_pedidos)):
+        print(lista_pedidos[i])
+
 
 def consultar_pedidos(lista_pedidos):
     id_pedido = input("Digite o Id do pedido: ")
