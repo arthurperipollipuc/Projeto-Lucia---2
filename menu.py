7a43289a8fe4fdc70745f9f6dda503eb17f80a0b
arthurperipollipuc <arthur.ps3@puccampinas.edu.br>
2026-05-25 11:24:56 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,6 +5,8 @@ pedidos=[]
 entregadores=[]
 operacoes=[]
 
+
+
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -56,7 +58,8 @@ def gerenciamento_pedidos():
     print("1- Cadastrar Pedido")
     print("2- Listar Pedidos")
     print("3- Consultar Pedido")
-    print("4- Voltar")
+    print("4- Atualização do Pedido")
+    print("5- Voltar")
 
     opcao = int(input(f"\nSelecione uma opção: "))
     if opcao == 1:
@@ -65,6 +68,33 @@ def gerenciamento_pedidos():
         listar_pedidos()
     elif opcao == 3:
         consulta_pedido()
+    elif opcao == 4:
+        atualizar_pedido()
+
+def atualizar_pedido():
+    opcao=None
+    while opcao == None:
+        print(f"\n====ATUALIZAÇÃO====")
+        print("1- Alterar Status")
+        print("2- Cancelar Pedido")
+        print("3- Associar Entregador")
+        print("4- Remover Entregador")
+        print("5- Voltar")
+        o = int(input("Qual alteração deseja ser feita: "))
+        if(o==1):
+            funcoes.alt_status(pedidos)
+            principal()
+        elif(o==2):
+            funcoes.cancel_pedido(pedidos)
+        elif (o==3):
+            funcoes.ass_entregador(pedidos,entregadores)
+        elif (o==4):
+            pass
+        elif (o==5):
+            principal()
+        else:
+            print("Selecione uma das opções")
+            opcao== None
 
 
 def cadastro_pedidos():
